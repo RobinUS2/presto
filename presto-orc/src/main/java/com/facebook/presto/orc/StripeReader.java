800c5617ec3df46cb05d393319f22603ac167467
Robin Verlangen <robin+github@us2.nl>
2016-08-29 12:56:08 +0200

    Fix checkstyle for testing

--- presto-orc/src/main/java/com/facebook/presto/orc/StripeReader.java
+++ presto-orc/src/main/java/com/facebook/presto/orc/StripeReader.java
@@ -48,10 +48,7 @@ import io.airlift.slice.Slices;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
--- presto-orc/src/main/java/com/facebook/presto/orc/TupleDomainOrcPredicate.java
+++ presto-orc/src/main/java/com/facebook/presto/orc/TupleDomainOrcPredicate.java
@@ -31,7 +31,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import io.airlift.log.Logger;
 import io.airlift.slice.Slice;
-import org.apache.hive.common.util.BloomFilter;
 
 import java.util.Collection;
 import java.util.List;
@@ -119,7 +118,11 @@ public class TupleDomainOrcPredicate<C>
                 }
             }
             for (RowGroupBloomfilter rowGroupBloomfilter : bloomfilters) {
-//                BloomFilter.BitSet bs = new BloomFilter.BitSet(rowGroupBloomfilter.getBloomfilter().getBitsetCount());
+//                BitSet bs = new BitSet(rowGroupBloomfilter.getBloomfilter().getBitsetCount());
+//                for (int i = 0; i < rowGroupBloomfilter.getBloomfilter().getBitsetCount(); i++) {
+//                    bs.set(i);
+//                }
+//                BloomFilter bf = new BloomFilter();
                 // if bloom filter is matched here return true so we select this stripe as it likely contains data which we need to read
             }
         }
